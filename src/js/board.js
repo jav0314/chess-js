84985019c029e329016bb63bd8d8eabdddcaf4ef
jav0314 <javiermerida134@gmail.com>
2023-09-27 03:53:14 -0500

    black and white squares

--- src/js/board.js
+++ src/js/board.js
@@ -3,15 +3,26 @@ const lettersId = "ABCDEJGH";
 export default class Board {
   init() {
     const app = document.getElementById("app");
-    const frangment = document.createDocumentFragment();
+    const fragment = document.createDocumentFragment();
+    let isWhite = true;
+
     for (let row = 8; row >= 1; row--) {
       for (let column = 0; column < 8; column++) {
         const square = document.createElement("div");
         square.setAttribute("id", lettersId[column] + row);
         square.setAttribute("class", "square");
-        frangment.append(square);
+
+        if (isWhite){
+          square.classList.add("white-Square");
+        } else{
+          square.classList.add("black-Square");
+        }
+        fragment.append(square);
+        isWhite = !isWhite;
       }
+      isWhite = !isWhite;
     }
-    app.append(frangment);
+
+    app.append(fragment);
   }
 }
