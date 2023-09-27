dc424d959d60495e81ccd238fd5f03b854a8cf98
jav0314 <javiermerida134@gmail.com>
2023-09-27 02:59:38 -0500

    Rows and columns in order

--- src/js/board.js
+++ src/js/board.js
@@ -1,17 +1,17 @@
-const lettersId = "abcdefgh";
+const lettersId = "ABCDEJGH";
 
 export default class Board {
-    init() {
-        const app = document.getElementById("app")
-        const frangment = document.createDocumentFragment();
-        for (let index = 1; index <= 8; index++) {
-            for (let i = 1; i <= 8; i++) {
-                const square = document.createElement("div");
-                square.setAttribute("id", lettersId[index - 1] + (9 - index));
-                square.setAttribute("class", "square");
-                frangment.append(square);
-            }
-        }
-        app.append(frangment);
+  init() {
+    const app = document.getElementById("app");
+    const frangment = document.createDocumentFragment();
+    for (let row = 8; row >= 1; row--) {
+      for (let column = 0; column < 8; column++) {
+        const square = document.createElement("div");
+        square.setAttribute("id", lettersId[column] + row);
+        square.setAttribute("class", "square");
+        frangment.append(square);
+      }
     }
+    app.append(frangment);
+  }
 }
