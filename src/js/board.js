0e4d7c6ca6500629418d14e93ce79941640b1cb2
jav0314 <javiermerida134@gmail.com>
2023-09-27 00:27:09 -0500

    Invert the squares

--- src/js/board.js
+++ src/js/board.js
@@ -1,15 +1,15 @@
 const lettersId = "abcdefgh";
 
-export default class Board{
-    init(){
+export default class Board {
+    init() {
         const app = document.getElementById("app")
         const frangment = document.createDocumentFragment();
         for (let index = 1; index <= 8; index++) {
             for (let i = 1; i <= 8; i++) {
                 const square = document.createElement("div");
-                square.setAttribute("id", lettersId[index -1] + i);
+                square.setAttribute("id", lettersId[index - 1] + (9 - index));
                 square.setAttribute("class", "square");
-                frangment.append(square);                
+                frangment.append(square);
             }
         }
         app.append(frangment);
